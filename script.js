f996b413dcff96a4d9a2c3dd727325ea407488d3
Felix <owolabifelix78@gmail.com>
2024-09-22 18:54:21 +0100

    oof class

--- script.js
+++ script.js
@@ -19,6 +19,41 @@
 
    }
 
+   function changeDay(){
+      const dateElement = document.getElementById('dateText').innerHTML = 'Today is Sunday';
+      dateElement.style.fontSize = "20px";
+      dateElement.style.color = "green";
+      dateElement.style.fontWeight = '700';
+   }
+
+   function signUp(){
+      const displayElement = document.getElementById('displayName');
+      const firstName = document.getElementById('fname').value;
+      // const parsedName = Number(firstName)
+      const lastName = document.getElementById('lname').value;
+   
+      displayElement.innerHTML = `This is my name: ${firstName} ${lastName}`
+      // alert('This is my name:' + firstName + lastName)
+      // alert(`This is my name: ${firstName} ${lastName}`)
+      // console.log(firstName)
+      // console.log(lastName)
+
+   }
+
+
+
+   function addition (a,b){
+      var sum = a + b;
+      return sum ;
+
+      // return a +b ;
+   }
+
+  
+   console.log(addition(2,7))
+ 
+  
+
 
 
   
