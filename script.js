f60cadea97e96659ca64b7ab4d5c0c48191e772a
Felix <owolabifelix78@gmail.com>
2024-09-22 17:29:23 +0100

    github deployment

---
+++ script.js
@@ -0,0 +1,38 @@
+   
+   const studentRecords = [];
+
+   const student = {
+    "firstName": 'John',
+    "lastName": "Doe",
+     "id": "OOF-12"
+   }
+
+   studentRecords.push(student);
+
+   console.log(studentRecords)
+
+
+   function alertSomething(){
+      var add = 2 + 2
+      alert(add)
+    //   return add
+
+   }
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+// Index will always start counting from 0
+// Length counts from 1
