947e32c362da6c3180f3f730c581196feba3bc6d
Orisamika Samuel <satechmedia@gmail.com>
2024-09-28 14:08:16 +0100

    feat: update student records display and refactor code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
    
-   const studentRecords = [];
+   // const studentRecords = [];
 
    const student = {
     "firstName": 'John',
@@ -7,9 +7,9 @@
      "id": "OOF-12"
    }
 
-   studentRecords.push(student);
+   // studentRecords.push(student);
 
-   console.log(studentRecords)
+   // console.log(studentRecords)
 
 
    function alertSomething(){
@@ -53,8 +53,55 @@
    console.log(addition(2,7))
  
   
+// Javascript Array Methods
 
+const studentRecords = [
+   {
+      "firstName": "John",
+      "lastName": "Doe",
+      "id": "OOF-12",
+      "age": 20
+   },
+   {
+      "firstName": "Jane",
+      "lastName": "Doe",
+      "id": "OOF-13",
+      "age": 22
+   },
+   {
+      "firstName": "Sam",
+      "lastName": "Doe",
+      "id": "OOF-14",
+      "age": 20
 
+   }
+]
+
+// console.log(studentRecords[0].lastName,studentRecords[0].firstName)
+// Map Method
+// studentRecords.map()
+studentRecords.map((eachStudent)=>{
+  const displayElement =  document.getElementById('displayStudentRecords');
+   // displayElement.innerHTML += `<p>${eachStudent.firstName} ${eachStudent.lastName} ${eachStudent.id}</p>`
+   displayElement.innerHTML += `
+   <p>FirstName: ${eachStudent.firstName}</p>
+    <p>LastName: ${eachStudent.lastName}</p>
+   `
+
+})
+
+const filteredStudents = studentRecords.filter((eachStudent)=>{
+   return eachStudent.age === 20
+})
+
+console.log("Filtered Students",filteredStudents)
+
+// function studentsArray(){
+   
+// }
+// const studentArray =()=> console.log(student)
+
+console.log(studentRecords)
 
   
 
